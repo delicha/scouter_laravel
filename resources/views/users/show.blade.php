364afc29b5d7976fab2c0da6d3b9b8b401b79b33
delicha <shoheiyamamoto919@gmail.com>
2023-05-05 02:35:41 +0900

    認証されたユーザーと閲覧ユーザーが同じかどうかで表示を分岐。

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,10 +10,12 @@
 
             <div class="p-6">
                 <h2 class="text-2xl font-bold mb-2 text-center">{{ $user->name }}<span class="text-sm font-normal">さん</span></h2>
-                <div class="text-center mb-3">
-                    <i class="fa-solid fa-p fa-bounce text-3xl" style="color: orange;font-weight:bold;"></i>
-                    <span class="text-3xl px-3">{{ $point->point }}</span>ポイントを所持
-                </div>
+                @if($auth == $user)
+                    <div class="text-center mb-3">
+                        <i class="fa-solid fa-p fa-bounce text-3xl" style="color: orange;font-weight:bold;"></i>
+                        <span class="text-3xl px-3">{{ $point->point }}</span>ポイントを所持
+                    </div>
+                @endif
                 <h3 class="text-white bg-gray-400 p-1 pl-2 rounded-sm">Hobby</h3>
                 <p class="my-2">{{ $user->hobby }}</p>
                 <h3 class="text-white bg-gray-400 p-1 pl-2 rounded-sm">Character</h3>
@@ -40,7 +42,7 @@
                     </div>
                 </div>
             </div>
-            @if($auth)
+            @if($auth == $user)
                 <div class="flex justify-center m-3">
                     <x-secondary-button>
                         <a href="{{ route('profile.edit') }}">プロフィール編集</a>
@@ -53,9 +55,17 @@
                     </x-secondary-button>
                 </div>
             @endif
+            {{-- チケット制がよい。1日たつと無効 tickets table--}}
+            @if($point->point >= 3)
+                <div class="flex justify-center m-3">
+                    <x-secondary-button>
+                        <a href="{{ route('') }}">この人の評価を見る(3pt消化)</a>
+                    </x-secondary-button>
+                </div>
+            @endif
         </div>
     </div>
-    @if($auth)
+    @if($auth == $user)
         <div class="flex flex-row m-10 justify-between mb-10">
             <div class="bg-white rounded-lg shadow-lg card max-w-300 sm:max-w-250 overflow-hidden p-5">
                 <div class="text-center mb-2">
@@ -81,7 +91,7 @@
         </div>
         <div class="flex justify-center m-3">
             <x-secondary-button>
-                <a href="{{ route('evaluations.index') }}">自分を評価した人を見る</a>
+                <a href="{{ route('evaluations') }}">自分を評価した人を見る</a>
             </x-secondary-button>
         </div>
     @endif
