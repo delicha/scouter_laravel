98602c86460b456e63098388551a4fad65ec5094
delicha <shoheiyamamoto919@gmail.com>
2023-05-05 23:59:17 +0900

    formselectionを使って世代選択

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -69,9 +69,44 @@
             <x-input-error class="mt-2" :messages="$errors->get('character')" />
         </div>
 
+        {{-- <div>
+            <x-input-label for="name" :value="__('Generation')" />
+            <label for="generation">世代を選択してください:</label>
+            <select id="generation" name="generation" required autofocus autocomplete="generation">
+                <option value="1" {{ $user->generation == 1 ? 'selected' : '' }}>10代</option>
+                <option value="2" {{ $user->generation == 2 ? 'selected' : '' }}>20代</option>
+                <option value="3" {{ $user->generation == 3 ? 'selected' : '' }}>30代</option>
+                <option value="4" {{ $user->generation == 4 ? 'selected' : '' }}>40代</option>
+                <option value="5" {{ $user->generation == 5 ? 'selected' : '' }}>50代</option>
+                <option value="6" {{ $user->generation == 6 ? 'selected' : '' }}>60代</option>
+                <option value="7" {{ $user->generation == 7 ? 'selected' : '' }}>70代</option>
+                <option value="8" {{ $user->generation == 8 ? 'selected' : '' }}>80代</option>
+            </select>
+            <x-input-error class="mt-2" :messages="$errors->get('generation')" />
+        </div> --}}
+        @php
+            $generations = [
+                1 => '10代',
+                2 => '20代',
+                3 => '30代',
+                4 => '40代',
+                5 => '50代',
+                6 => '60代',
+                7 => '70代',
+                8 => '80代',
+            ];
+        @endphp
+
         <div>
-            <x-input-label for="generation" :value="__('Generation')" />
-            <x-text-input id="generation" name="generation" type="text" class="mt-1 block w-full" :value="old('generation', $user->generation)" required autofocus autocomplete="generation" />
+            <x-input-label for="name" :value="__('Generation')" />
+            <label for="generation">世代を選択してください:</label>
+            <select id="generation" name="generation" required autofocus autocomplete="generation">
+                @foreach($generations as $value => $label)
+                    <option value="{{ $value }}" {{ $user->generation == $value ? 'selected' : '' }}>
+                        {{ $label }}
+                    </option>
+                @endforeach
+            </select>
             <x-input-error class="mt-2" :messages="$errors->get('generation')" />
         </div>
 
